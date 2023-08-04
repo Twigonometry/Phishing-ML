22045262dd22356dd6fa88f9968b0b4ee9d8c2a6
Twigonometry <mac.j.goodwin@gmail.com>
2023-08-04 19:39:44 +0100

    Add ratings to schema

--- datalabelling/datalabelling/app.js
+++ datalabelling/datalabelling/app.js
@@ -29,11 +29,17 @@ const Schema = mongoose.Schema;
 
 const EmailSchema = new Schema({
   body: String,
+  ratings: {
+    authoritative: Number,
+    threatening: Number,
+    rewarding: Number,
+    unnatural: Number,
+    emotional: Number,
+    provoking: Number,
+    timesensitive: Number,
+    imperative: Number
+  }
 });
-
-// const RatingSchema = new Schema({
-//   email: id
-// })
 
 var app = express();
 
