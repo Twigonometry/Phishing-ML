673a067fbc69806e2cb10e60d0fad40aba4f570d
Twigonometry <mac.j.goodwin@gmail.com>
2023-08-04 18:51:05 +0100

    Some database configuration

--- datalabelling/datalabelling/app.js
+++ datalabelling/datalabelling/app.js
@@ -7,6 +7,34 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Import the mongoose module
+const mongoose = require("mongoose");
+
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+// Included because it removes preparatory warnings for Mongoose 7.
+// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+mongoose.set("strictQuery", false);
+
+// Define the database URL to connect to.
+const mongoDB = "mongodb://127.0.0.1/my_database";
+
+// Wait for database to connect, logging an error if there is a problem
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+// Define a schema
+const Schema = mongoose.Schema;
+
+const EmailSchema = new Schema({
+  body: String,
+});
+
+// const RatingSchema = new Schema({
+//   email: id
+// })
+
 var app = express();
 
 // view engine setup
